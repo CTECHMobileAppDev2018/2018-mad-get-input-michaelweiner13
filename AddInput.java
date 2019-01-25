4f41af469b5ab9150cd1b33ae124846ba24189da
Michael Weiner <46925721+michaelweiner13@users.noreply.github.com>
2019-01-25 10:47:00 -0600

    Update AddInput.java

--- AddInput.java
+++ AddInput.java
@@ -1,11 +1,5 @@
 public class AddInput { 
     public static void main(String[] args) {
-    
-        // Advanced section (once you get the sum working):
-        // If you want to make sure that the user entered EXACTLY two integers, you need to get the number of inputs
-        // The variable "args" holds all of the arguments that were passed into this program
-        // The variable "args" is called an "array" because it's actually a list of items
-        // If you good "How to get the number of items in an array in Java", you should be able to figure out how many integers there are in the variable "args"
         
         // Creates and initalizes two boolean variabes to report to the program if the array args[] contains all ints by checking the values of args[1] and args[2]
         // This is not done recursivly because a check is in place to make sure the array stores only 2 values
