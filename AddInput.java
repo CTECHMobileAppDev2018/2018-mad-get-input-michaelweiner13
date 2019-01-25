dcb52d3851e1c75bc2f9fa9d8f3240897849448c
Michael <6993102@isd535.org>
2019-01-25 08:35:17 -0600

    Update AddInput.java

--- AddInput.java
+++ AddInput.java
@@ -1,6 +1,4 @@
-import java.util.Scanner;
 public class AddInput { 
-
     public static void main(String[] args) {
     
         // Advanced section (once you get the sum working):
@@ -9,17 +7,18 @@ public class AddInput {
         // The variable "args" is called an "array" because it's actually a list of items
         // If you good "How to get the number of items in an array in Java", you should be able to figure out how many integers there are in the variable "args"
         // int num_args = ???
-        // if (??? != 2) {
-        //    do something
-        // }
-    
-        // **** START HERE: ****
-        // These two lines will get you the first two arguments 
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int sum = a + b;
+      
+        if (args.length != 2 ) {
+            System.out.println("You must enter exactly two integers!");
+        } else {
+          // **** START HERE: ****
+          // These two lines will get you the first two arguments 
+          int a = Integer.parseInt(args[0]);
+          int b = Integer.parseInt(args[1]);
+          int sum = a + b;
         
-        // Print out "The sum of _ and _ is _" where you actually print the numbers they enter and the sum
-        System.out.println("The sum of " + a + " and " + b + " is " + sum + ".");
+          // Print out "The sum of _ and _ is _" where you actually print the numbers they enter and the sum
+          System.out.println("The sum of " + a + " and " + b + " is " + sum + ".");
+        }
     }
 }
